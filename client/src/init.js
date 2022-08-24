9c865f74081fa9f4e759b6e45e9c2b7fb88bc673
minju <minjupgeorge@gmail.com>
2022-08-25 01:46:49 +0200

    phone and email added

--- client/src/init.js
+++ client/src/init.js
@@ -16,16 +16,24 @@ function myFunction() {
     
     function appendData(data) 
     {
-        const newol = document.createElement("ol")
+        const newol = document.createElement("ul")
         for (var i = 0; i < data.length; i++) {
         const newLi= document.createElement("li");
         newLi.innerText =  data[i].name;
+        const newLi1= document.createElement("li");
+        newLi1.innerText =  data[i].phone;
+        const newLi2= document.createElement("li");
+        newLi2.innerText =  data[i].email;
+        const newLi3= document.createElement("li");
+        newLi3.innerText =  data[i].address.city;
+        newol.appendChild(newLi) ;  
+        newol.appendChild(newLi1) ;
+        newol.appendChild(newLi2) ;
+        newol.appendChild(newLi3) ;
+        }
          
-        newol.appendChild(newLi)
-         }
-        
-        
-        root.append(newol);
+                root.append(newol);
+        }
     }
-}
+
 
